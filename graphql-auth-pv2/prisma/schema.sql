913f4a45f6076cea0464902b31cecc2fd1358bc5
cq-m-ali <muhammad.ali@cqtechnologies.com>
2020-09-09 09:32:26 +0500

    graphql-auth-pv2

---
+++ graphql-auth-pv2/prisma/schema.sql
@@ -0,0 +1,22 @@
+CREATE TABLE IF NOT EXISTS "public"."User" (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS "public"."Post" (
+  id SERIAL PRIMARY KEY NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  "createdAt" TIMESTAMP NOT NULL DEFAULT now(),
+  content TEXT NOT NULL,
+  published BOOLEAN NOT NULL DEFAULT false,
+  "authorId" INTEGER NOT NULL,
+  FOREIGN KEY ("authorId") REFERENCES "public"."User"(id)
+);
+
+CREATE TABLE IF NOT EXISTS "public"."Profile" (
+  id SERIAL PRIMARY KEY NOT NULL,
+  bio TEXT,
+  "userId" INTEGER UNIQUE NOT NULL,
+  FOREIGN KEY ("userId") REFERENCES "public"."User"(id)
+);
